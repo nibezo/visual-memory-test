a4e34ad242e51741869426cc96741604ee951bf6
Ilya Yudin <nibezo.cs@gmail.com>
2022-05-20 02:01:26 +0300

    feat: from 2x2 matrix to 4x4

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ var btn_1 = document.getElementById('btn_1');
 var btn_2 = document.getElementById('btn_2');
 var btn_3 = document.getElementById('btn_3');
 var btn_4 = document.getElementById('btn_4');
+var btn_5 = document.getElementById('btn_5');
+var btn_6 = document.getElementById('btn_6');
+var btn_7 = document.getElementById('btn_7');
+var btn_8 = document.getElementById('btn_8');
+var btn_9 = document.getElementById('btn_9');
+var btn_10 = document.getElementById('btn_10');
+var btn_11 = document.getElementById('btn_11');
+var btn_12 = document.getElementById('btn_12');
+var btn_13 = document.getElementById('btn_13');
+var btn_14 = document.getElementById('btn_14');
+var btn_15 = document.getElementById('btn_15');
+var btn_16 = document.getElementById('btn_16');
+
 
 btn_1.addEventListener("click", function() {
   this.classList.add("active");
@@ -13,5 +26,41 @@ btn_3.addEventListener("click", function() {
   this.classList.add("active");
 });
 btn_4.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_5.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_6.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_7.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_8.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_9.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_10.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_11.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_12.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_13.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_14.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_15.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_16.addEventListener("click", function() {
   this.classList.add("active");
 });
