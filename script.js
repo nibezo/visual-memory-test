51a0f0c3a269749f2a737ffd1d2fab34696d9111
Ilya Yudin <nibezo.cs@gmail.com>
2022-05-20 01:49:51 +0300

    feat: add change color in JS

---
+++ script.js
@@ -0,0 +1,17 @@
+var btn_1 = document.getElementById('btn_1');
+var btn_2 = document.getElementById('btn_2');
+var btn_3 = document.getElementById('btn_3');
+var btn_4 = document.getElementById('btn_4');
+
+btn_1.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_2.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_3.addEventListener("click", function() {
+  this.classList.add("active");
+});
+btn_4.addEventListener("click", function() {
+  this.classList.add("active");
+});
